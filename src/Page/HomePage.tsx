14efde3ee4c3ea5d76500b993f864d8a4fb84d37
Harby Garcia <harby-96@hotmail.com>
2025-05-27 13:27:22 -0500

    mostrando la url correcta

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage: React.FC = () => {
     if (code) {
       navigate(`/login?code=${code}`);
     } else {
-      window.location.href = 'https://us-east-1zu66egizv.auth.us-east-1.amazoncognito.com/login?client_id=18ojcokska1igo3hrb743d6bt5&redirect_uri=https%3A%2F%2Fdevelop.d3d4ljvzw46psr.amplifyapp.com%2F&response_type=code&scope=email+openid+phone';
+      window.location.href = 'https://us-east-1zu66egizv.auth.us-east-1.amazoncognito.com/login?client_id=18ojcokska1igo3hrb743d6bt5&redirect_uri=https%3A%2F%2Fdevelop.d3d4ljvzw46psr.amplifyapp.com%2Fdashboard%2F&response_type=code&scope=email+openid+phone';
     }
   }, [code, navigate]);
  
